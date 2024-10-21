a8bdac6a2e75e2a9cd8f11281aec21f7023a2f47
Buluma <alberbuluma76@gmail.com>
2024-10-21 18:56:55 +0300

    initial commit

---
+++ queries.sql
@@ -0,0 +1,18 @@
+-- Avarage salary per department
+ SELECT Department, AVG(MonthlyIncome) avg_monthly_income
+   FROM employeedata
+  GROUP BY Department
+  ORDER BY avg_monthly_income DESC;
+  
+-- Average Monthly income by Education field
+ SELECT EducationField, AVG(MonthlyIncome) avg_monthly_income
+   FROM employeedata
+  GROUP BY EducationField
+  ORDER BY avg_monthly_income DESC;
+  
+  SELECT ed.YearsWithCurrManager, AVG(es.JobSatisfaction) avgjobsatisfaction
+    FROM employeedata ed
+   INNER JOIN employee_survey es
+   GROUP BY ed.YearsWithCurrManager
+   ORDER BY avgjobsatisfaction;
+   
