2f04bad7e806b0c70e421607946f8bf73281c938
Buluma <alberbuluma76@gmail.com>
2024-10-22 18:47:41 +0300

    EDA commit

--- queries.sql
+++ queries.sql
@@ -17,6 +17,136 @@
    ORDER BY avgjobsatisfaction;
    
 -- Attrition analysis
-SELECT * FROM employeedata
+SELECT Age, JobLevel, JobRole, Department, DistanceFromHome, 
+       Gender, NumCompaniesWorked, PercentSalaryHike
+  FROM employeedata
  WHERE Attrition = "Yes";
+ 
+SELECT a.Department, COUNT(*)
+  FROM (SELECT Department
+		  FROM employeedata
+		 WHERE Attrition = "Yes") a
+ GROUP BY Department;
+ 
+SELECT a.JobLevel, COUNT(*) numofemployees
+  FROM (SELECT JobLevel
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+  GROUP BY JobLevel
+  ORDER BY JobLevel ASC;
+  
+  SELECT a.JobRole, COUNT(*) numofemployees
+  FROM (SELECT JobRole
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+  GROUP BY JobRole
+  ORDER BY numofemployees DESC;
+  
+SELECT a.DistanceFromHome, COUNT(*) numofemployees
+  FROM (SELECT DistanceFromHome
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY DistanceFromHome
+ ORDER BY numofemployees DESC;
+
+
+SELECT a.Age, COUNT(*) numofemployees
+  FROM (SELECT Age
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY Age
+ ORDER BY Age;
+ 
+
+-- education field
+SELECT a.EducationField, COUNT(*) numofemployees
+  FROM (SELECT EducationField
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY EducationField
+ ORDER BY numofemployees DESC;
+ 
+ -- education level
+ SELECT a.Education, COUNT(*) numofemployees
+  FROM (SELECT Education
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY Education
+ ORDER BY Education;
+ 
+ -- Years since last promotion
+SELECT a.YearsSinceLastPromotion, COUNT(*) numofemployees
+  FROM (SELECT YearsSinceLastPromotion
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY YearsSinceLastPromotion
+ ORDER BY YearsSinceLastPromotion;
+ 
+ -- Years at company
+SELECT a.YearsAtCompany, COUNT(*) numofemployees
+  FROM (SELECT YearsAtCompany
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY YearsAtCompany
+ ORDER BY numofemployees DESC;
+ 
+
+-- Years with current manager
+SELECT a.YearsWithCurrManager, COUNT(*) numofemployees
+  FROM (SELECT YearsWithCurrManager
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY YearsWithCurrManager
+ ORDER BY YearsWithCurrManager;
+ 
+-- Total working years
+SELECT a.TotalWorkingYears, COUNT(*) numofemployees
+  FROM (SELECT TotalWorkingYears
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY TotalWorkingYears
+ ORDER BY TotalWorkingYears;
+ 
+ -- Gender
+ SELECT a.Gender, COUNT(*) numofemployees
+  FROM (SELECT Gender
+		  FROM employeedata
+		 WHERE Attrition = 'Yes') a
+ GROUP BY Gender
+ ORDER BY numofemployees;
+ 
+ -- Demographic ensights
+ 
+ SELECT Department, EducationField, JobRole, JobLevel, Gender
+   FROM employeedata;
+ 
+-- employee count by department
+SELECT Department,Gender, COUNT(*) numofemployees
+  FROM employeedata
+ GROUP BY Department, Gender
+ ORDER BY numofemployees;
+ 
+ -- employee count by education field
+SELECT EducationField,Gender, COUNT(*) numofemployees
+  FROM employeedata
+ GROUP BY EducationField, Gender
+ ORDER BY numofemployees;
+ 
+ -- employee count by Job Role
+ SELECT JobRole,Gender, COUNT(*) numofemployees
+  FROM employeedata
+ GROUP BY JobRole, Gender
+ ORDER BY numofemployees;
    
+ -- employee count by Job Level
+ SELECT JobLevel,Gender, COUNT(*) numofemployees
+  FROM employeedata
+ GROUP BY JobLevel, Gender
+ ORDER BY JobLevel, Gender, numofemployees;
+ 
+ -- Compensation Analysis
+ 
+ SELECT Department, AVG(MonthlyIncome) AvgMonthlyincome
+   FROM employeedata
+  GROUP BY Department
+  ORDER BY AvgMonthlyincome;
